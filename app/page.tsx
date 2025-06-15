64444a0ed9f317f890a4cd74b879935210adc9bc
igbaroola samuel adebayo <igbaroola338@gmail.com>
2025-06-15 22:15:19 +0100

    added files

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+import { Heading, Text } from '@radix-ui/themes';
+
+export default function Home() {
+  return (
+    <main>
+      <div>
+        <Heading as="h1" size="8">Hello Friends</Heading>
+        <br />
+        <Text size="4">Move to the navbar and click issue</Text>
+      </div>
+    </main>
+  );
+}
